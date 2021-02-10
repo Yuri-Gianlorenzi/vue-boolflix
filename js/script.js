ac8dfd5f9096fce3f444247cc9c244225b94c9c7
Yuri Gianlorenzi <yurigian@gmail.com>
2021-02-10 23:58:14 +0100

    add milestone 1

---
+++ js/script.js
@@ -0,0 +1,62 @@
+// Attraverso una chiamata ajax all’Api di boolean avremo a disposizione una decina di dischi musicali.
+// Stampiamo tutto a schermo, in questo momento non è importante la parte grafica.
+// Bonus:
+// Creare una select con i seguenti generi: pop, rock, metal e jazz.
+// In base a cosa scegliamo nella select vedremo i corrispondenti cd.
+
+let app = new Vue({
+  el : '#app',
+  data : {
+    query : '',
+    apiKey : '1c2bfcf8efd8a513cb5a624bbe643c1b',
+    filmSearched : ''
+  },//fine data
+
+  mounted() {
+
+
+
+
+
+
+
+
+  },//fine mounted
+
+  methods : {
+
+    searchMovie() {
+      axios
+        .get("https://api.themoviedb.org/3/search/movie", {
+          params : {
+            api_key: this.apiKey,
+            query : this.query,
+            language : 'it-IT'
+          }
+        })
+        .then((result) => {
+          // console.log(result.data.results);
+          this.filmSearched = result.data.results;
+          // console.log(this.filmSearched);
+        })//fine then
+        .catch((error) => alert('errore'));
+        console.log(this.filmSearched);
+
+    }//fine searchMovie
+
+  }//fine methods
+});//fine vue
+
+
+
+
+
+
+
+
+
+
+
+
+// API
+// https://flynn.boolean.careers/exercises/api/array/music
